a027a2e5d6461a4a776adc06534dc79c7ae77652
Matias Montes <matias@semexpert.com.ar>
2018-12-10 18:01:43 -0300

    Added a block to handle hello bar output

---
+++ Block/CMS.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Barbazul
+ * Date: 10/12/2018
+ * Time: 5:57 PM
+ */
+
+namespace SemExpert\HelloBar\Block;
+
+/**
+ * Class CMS
+ * @package SemExpert\HelloBar\Block
+ * @api
+ */
+class CMS extends Magento\Cms\Block\Block
+{
+
+}
---
+++ Test/Unit/SemExpert/HelloBar/Block/CMSTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Barbazul
+ * Date: 10/12/2018
+ * Time: 5:59 PM
+ */
+
+namespace SemExpert\HelloBar\Block;
+
+use PHPUnit\Framework\TestCase;
+
+class CMSTest extends TestCase
+{
+
+}
